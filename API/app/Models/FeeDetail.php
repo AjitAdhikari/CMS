f87ae740e34ff6c0c13a7c1e553d5367dc3d3c5e
AashishBarme <aashish.barme9@gmail.com>
2025-12-09 20:38:32 +0545

    feat: added the fee and fee details model

---
+++ API/app/Models/FeeDetail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FeeDetail extends Model
+{
+    //
+    protected $fillable = [
+        'user_id',
+        'semester',
+        'payment_date',
+        'amount',
+        'created_by',
+        'updated_by'
+    ];
+
+     /**
+     * Relationship: FeeDetail belongs to User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
